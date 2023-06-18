fb142b5b5065fe4763fa901ff3cb40e5d5156f0a
Wajid Najeeb <136959728+WN253@users.noreply.github.com>
2023-06-18 20:50:45 +0530

    Update App.js

--- React/student_application/src/App.js
+++ React/student_application/src/App.js
@@ -6,23 +6,20 @@ import './App.css';
 function App() {
   return (
 
-<div className="container">
-  <h1>STUDENT APPLICATION FORM</h1>
-<div class="row">
+  <div className="container">
+    <h1>STUDENT APPLICATION FORM</h1>
+  <div className="row">
 
-
-  <div id="studentform" class="col-sm-4">
+  <div id="studentform" className="col-sm-4">
     <Student_form />
   </div>
 
-  <div id="studenttable" class="col-sm-8">
+  <div id="studenttable" className="col-sm-8">
     <Student_table />
   </div>
   
 </div>
 </div>
-
-
 
   );
 }
